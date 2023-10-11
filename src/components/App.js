67ad20df72da10af48398e6e469de586e6ba9bfb
Sean O'Grady <ogrady.sean.a@gmail.com>
2023-10-11 12:43:35 -0400

    Comment deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,19 @@ import React from "react";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
+/*
+DELIVERABLES
+------------
+- [ ] Load in all plants when app starts (GET)
+- [ ] Implement form to add new plants (POST)
+- [ ] Mark plant as "sold out" 
+- [ ] Search for plants by name, filter list
+ADVANCED
+--------
+- [ ] Update the price of a plant (PATCH)
+- [ ] Delete a plant (DELETE)
+*/
+
 function App() {
   return (
     <div className="app">
